1f7e6a6bc13ec065bbe0764e740ab73027742592
MonteDeveloper <lorenzomontello83@gmail.com>
2023-05-18 23:44:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MyTitle from './components/AppTitle.vue'
+
+export default {
+  components: {
+    MyTitle
+  }
+}
+</script>
+
+<template>
+  <MyTitle/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "AppTitle",
+}
+</script>
+
+<template>
+    <h1 class="green">La mia prima app con <span class="viteText">Vite!</span></h1>
+</template>
+
+<style scoped>
+h1 > span.viteText {
+    color: aqua;
+    text-decoration: underline;
+}
+</style>
